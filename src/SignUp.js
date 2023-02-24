c9c0ed4f37659b0544de3139823b492f049b53b4
Cian Gonzales <ciangonzales10@gmail.com>
2023-02-24 13:15:21 -0800

    User-registration through signup page to firebase
    Base sign up page will serve as a template for the sign in page. User authentication allows for distinguishing between users and setting up later implementations like profile pages, user requests, etc.

--- src/SignUp.js
+++ src/SignUp.js
@@ -1,35 +1,83 @@
-import React , {useRef} from 'react';
-import { Form, Button, Card} from 'react-bootstrap'
 
-export default function SignUp() {
-    const emailRef = useRef()
-    const passwordRef = useRef()
-    const passwordConfirmRef = useRef()
-    return (
-        <>
-            <Card>
-                <Card.Body>
-                    <h2 className='text-center mb-4'>Sign Up</h2>
-                    <Form>
-                        <Form.Group id='email'>
-                            <Form.Label>Email</Form.Label>
-                            <Form.Control type='email' ref={emailRef} required />
-                        </Form.Group>
-                        <Form.Group id='password'>
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control type='password' ref={passwordRef} required />
-                        </Form.Group>
-                        <Form.Group id='password-confirm'>
-                            <Form.Label>Password Confirmation</Form.Label>
-                            <Form.Control type='password' ref={passwordConfirmRef} required />
-                        </Form.Group>
-                        <Button classname='w-100' type='submit'>Sign Up</Button>
-                    </Form>
-                </Card.Body>
-            </Card>
-            <div className='w-100 text-center mt-2'>
-                Already have an account? Log in
-            </div>
-        </>
-    )
+import React, { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { Link, useNavigate } from "react-router-dom";
+import { auth , database } from "./firebase";
+import {collection , addDoc } from "firebase/firestore"
+import { createUserWithEmailAndPassword , signOut } from "firebase/auth";
+import "./Signup.css";
+
+const registerWithEmailAndPassword = async (name, email, password) => {
+  try {
+    const res = await createUserWithEmailAndPassword(auth, email, password);
+    const user = res.user;
+    await addDoc(collection(database, "users"), {
+      uid: user.uid,
+      name,
+      authProvider: "local",
+      email,
+    });
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
 }
+
+function Signup() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, loading, error] = useAuthState(auth);
+
+  const navigate = useNavigate();
+
+  const register = () => {
+    //if (!email) alert("Please enter email");
+    registerWithEmailAndPassword(name, email, password); 
+  };
+
+  useEffect(() => {
+    if (loading) return;
+    if (user) {
+      navigate('/');
+    }
+   }, [user, loading]);
+
+  return (
+    <div className="register">
+      <div className="register__container">
+        <input
+          type="text"
+          className="register__textBox"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Full Name"
+        />
+        <input
+          type="text"
+          className="register__textBox"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="E-mail Address"
+        />
+        <input
+          type="password"
+          className="register__textBox"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+        />
+          <button className="register__btn" onClick={register}>Register</button>
+        <div>
+          Already have an account? <Link to="/sign-in">Login</Link> now.
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Signup;
+
+// No way to logout yet
+const logout = () => {
+  signOut(auth);
+};
