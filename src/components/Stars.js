170ebb9c75420ad9088ad84e6b0fc630d4fbbf9e
Cian Gonzales <ciangonzales10@gmail.com>
2023-03-12 22:36:31 -0700

    Finished review uploading feature to database
    Figured out way to send both the star rating and the contents
    of the review to the database under the same document.

--- src/components/Stars.js
+++ src/components/Stars.js
@@ -1,19 +1,50 @@
+// import React, { useState } from 'react';
+// import Rating from '@mui/material/Rating';
+// import Box from '@mui/material/Box';
+
+// const RatingSystem = () => {
+//   const [value, setValue] = useState(0);
+
+//   const handleRatingChange = (event, newValue) => {
+//     const intValue = parseInt(newValue, 10);
+//     setValue(intValue);
+//     handleSelectedValue(intValue);
+//   };
+
+//   const handleSelectedValue = (selectedValue) => {
+//     // Do something with the selected value, such as updating state or sending to a server
+//     console.log('Selected value:', selectedValue);
+//   };
+
+//   return (
+//     <Box 
+//      sx={{ 
+//       display: 'flex',
+//       justifyContent: 'flex-start',
+//       '& > *': { m: 1 } 
+//     }}>
+//       <Rating
+//         name="simple-controlled"
+//         value={value}
+//         onChange={handleRatingChange}
+//       />
+//     </Box>
+//   );
+// }
+
+// export default RatingSystem;
+
 import React, { useState } from 'react';
 import Rating from '@mui/material/Rating';
 import Box from '@mui/material/Box';
 
-const RatingSystem = () => {
+const RatingSystem = ({ onRatingChange }) => {
   const [value, setValue] = useState(0);
 
   const handleRatingChange = (event, newValue) => {
     const intValue = parseInt(newValue, 10);
     setValue(intValue);
-    handleSelectedValue(intValue);
-  };
-
-  const handleSelectedValue = (selectedValue) => {
-    // Do something with the selected value, such as updating state or sending to a server
-    console.log('Selected value:', selectedValue);
+    onRatingChange(intValue);
   };
 
   return (
--- src/components/TextBox.js
+++ src/components/TextBox.js
@@ -1,4 +1,41 @@
-import React from 'react';
+// import React from 'react';
+// import './TextBox.css'
+// import SendIcon from '@mui/icons-material/Send';
+// import IconButton from '@mui/material/IconButton';
+// import RatingSystem from './Stars';
+// import { upload } from '@testing-library/user-event/dist/upload';
+// import { uploadComment } from '../Upload'
+
+
+
+// const TextBox = ({ placeholderText }) => {
+//   return (
+//     <>
+//     <div className="text-box-container">
+//         <div className="text-box-submission">
+//             <RatingSystem className="text-box-stars"><text>Rate this Bruin Spot here!</text></RatingSystem>
+//             <textarea type="text" className="text-box" id="my-text-area" placeholder={placeholderText}>
+//             </textarea>
+//             <IconButton className='submit-button' onClick={handleClickSend}><SendIcon></SendIcon></IconButton>
+//         </div>
+//     </div>
+//     </>
+//   );
+// };
+// function handleClickSend({e})
+// {
+//   const textArea = document.getElementById('my-text-area');
+//   const userText = textArea.value;
+//   const url = window.location.href;
+//   const locID = url.substring(url.lastIndexOf('/') + 1);
+//   console.log(url);
+//   console.log(locID);
+//   uploadComment(userText, 5, "Gd0AOQIGE1VLpOI793CK94pDRRz1" , locID);
+//   return;
+// }
+// export default TextBox;
+
+import React, { useState } from 'react';
 import './TextBox.css'
 import SendIcon from '@mui/icons-material/Send';
 import IconButton from '@mui/material/IconButton';
@@ -9,11 +46,29 @@ import { uploadComment } from '../Upload'
 
 
 const TextBox = ({ placeholderText }) => {
+  const [selectedRating, setSelectedRating] = useState(0);
+
+  const handleRatingChange = (ratingValue) => {
+    setSelectedRating(ratingValue);
+    console.log('Selected rating value:', ratingValue);
+  };
+
+  const handleClickSend = (e) => {
+    const textArea = document.getElementById('my-text-area');
+    const userText = textArea.value;
+    const url = window.location.href;
+    const locID = url.substring(url.lastIndexOf('/') + 1);
+    console.log(url);
+    console.log(locID);
+    uploadComment(userText, selectedRating, "Gd0AOQIGE1VLpOI793CK94pDRRz1" , locID);
+    return;
+  };
+
   return (
     <>
     <div className="text-box-container">
         <div className="text-box-submission">
-            <RatingSystem className="text-box-stars"><text>Rate this Bruin Spot here!</text></RatingSystem>
+            <RatingSystem onRatingChange={handleRatingChange} className="text-box-stars"><text>Rate this Bruin Spot here!</text></RatingSystem>
             <textarea type="text" className="text-box" id="my-text-area" placeholder={placeholderText}>
             </textarea>
             <IconButton className='submit-button' onClick={handleClickSend}><SendIcon></SendIcon></IconButton>
@@ -22,15 +77,6 @@ const TextBox = ({ placeholderText }) => {
     </>
   );
 };
-function handleClickSend({e})
-{
-  const textArea = document.getElementById('my-text-area');
-  const userText = textArea.value;
-  const url = window.location.href;
-  const locID = url.substring(url.lastIndexOf('/') + 1);
-  console.log(url);
-  console.log(locID);
-  uploadComment(userText, 5, "Gd0AOQIGE1VLpOI793CK94pDRRz1" , locID);
-  return;
-}
+
 export default TextBox;
+
